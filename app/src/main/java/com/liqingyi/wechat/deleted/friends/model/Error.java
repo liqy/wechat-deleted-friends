8ee998a957a0245e9fd069a6453246b020d3559b
liqy <liqingyi@outlook.com>
2016-01-06 23:00:22 +0800

    修改配置文件

--- app/src/main/java/com/liqingyi/wechat/deleted/friends/model/Error.java
+++ app/src/main/java/com/liqingyi/wechat/deleted/friends/model/Error.java
@@ -1,33 +1,22 @@
 package com.liqingyi.wechat.deleted.friends.model;
 
-import org.simpleframework.xml.Element;
-import org.simpleframework.xml.Root;
-
 /**
  * Created by liqin on 2016/1/6.
  */
-@Root
 public class Error {
 
-    @Element
     public int ret;
 
-    @Element
     public String message;
 
-    @Element
     public String skey;
 
-    @Element
     public String wxsid;
 
-    @Element
     public String wxuin;
 
-    @Element
     public String pass_ticket;
 
-    @Element
     public int isgrayscale;
 
     public Error() {
